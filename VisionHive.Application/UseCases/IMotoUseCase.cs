568915ce021db175f043ba0bfdf36aba42316dd8
larissa557197 <larissampmuniz@gmail.com>
2025-09-23 15:01:22 -0300

    feat(moto): add IMotoUseCase interface (CRUD + pagination)

--- VisionHive.Application/UseCases/IMotoUseCase.cs
+++ VisionHive.Application/UseCases/IMotoUseCase.cs
@@ -1,6 +1,22 @@
-﻿namespace VisionHive.Application.UseCases;
+﻿using VisionHive.Application.DTO.Request;
+using VisionHive.Domain.Entities;
+using VisionHive.Domain.Pagination;
 
+namespace VisionHive.Application.UseCases;
+
+// contrato de operações da entidade moto
+// define métodos de CRUD e paginação para uso pelos controllers
 public interface IMotoUseCase
 {
+    // retorna lista paginada de motos com filtros e ordenação
+    Task<PageResult<Moto>> GetPagination(MotoPaginatedRequest request);
     
+    // busca uma moto específica pelo ID
+    Task<Moto?> CreateAsync(MotoRequest request);
+    
+    // atualiza uma moto existente
+    Task<bool> UpdateAsync(Guid id, MotoRequest request);
+    
+    // remove uma moto pelo seu ID
+    Task<bool> DeleteAsync(Guid id);
 }
