5763507dec9aaec87bfce9168ddc9f0d0ef85de9
larissa557197 <larissampmuniz@gmail.com>
2025-09-23 14:17:28 -0300

    feat(moto): add MotoRequest DTO with DataAnottations and XML docs

--- VisionHive.Application/DTO/Request/MotoRequest.cs
+++ VisionHive.Application/DTO/Request/MotoRequest.cs
@@ -1,12 +1,25 @@
-﻿using VisionHive.Application.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using VisionHive.Application.Enums;
 namespace VisionHive.Application.DTO.Request
 {
     public class MotoRequest
     {
+        // placa da moto
         public string? Placa { get; set; }
+        
+        // chassi da moto
         public string? Chassi { get; set; }
+        
+        // número do motor da moto
         public string? NumeroMotor { get; set; }
+        
+        
+        // prioridade operacional para tratamento da moto
+        [Required]
         public Prioridade Prioridade { get; set; }
+        
+        // identificador do pátio onde a moto está vinculada
+        [Required]
         public Guid PatioId { get; set; }
     }
 }
