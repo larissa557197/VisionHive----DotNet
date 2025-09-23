0c748c9decb4f198ff8a3c841f23e34f9da65bb1
larissa557197 <larissampmuniz@gmail.com>
2025-09-23 20:43:44 -0300

    feat(app): add AddApplication extension (use cases registration)

---
+++ VisionHive.Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using VisionHive.Application.UseCases;
+
+namespace VisionHive.Application;
+
+public static class DependencyInjection
+{
+    /// <summary>
+    /// Registra os casos de uso da aplicação (camada Application).
+    /// </summary>
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IMotoUseCase, MotoUseCase>();
+        services.AddScoped<IPatioUseCase, PatioUseCase>();
+        services.AddScoped<IFilialUseCase, FilialUseCase>();
+        return services;
+    }
+}
