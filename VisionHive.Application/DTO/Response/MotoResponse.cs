e24447097dfee9aa8907171dd234420df8e202d4
larissa557197 <larissampmuniz@gmail.com>
2025-09-24 09:59:38 -0300

    refactor(moto): include Patio name in MotoResponse mapping

--- VisionHive.Application/DTO/Response/MotoResponse.cs
+++ VisionHive.Application/DTO/Response/MotoResponse.cs
@@ -2,11 +2,12 @@
 {
     public class MotoResponse
     {
-        public Guid Id { get; set; }
-        public string? Placa { get; set; }
-        public string? Chassi { get; set; }
-        public string? NumeroMotor { get; set; }
-        public string Prioridade { get; set; }
-        public string Patio { get; set; }
+        public Guid Id { get; init; }
+        public string? Placa { get; init; }
+        public string? Chassi { get; init; }
+        public string? NumeroMotor { get; init; }
+        public string Prioridade { get; init; }
+        public Guid PatioId { get; init; }
+        public string? Patio { get; init; }
     }
 }
