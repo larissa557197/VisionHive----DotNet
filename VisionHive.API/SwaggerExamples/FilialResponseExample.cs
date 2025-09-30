9f6fe334841b2ab4ebd1c2ca8c719655cb7d469f
larissa557197 <larissampmuniz@gmail.com>
2025-09-30 09:42:25 -0300

    feat(swagger): add request/response example providers - Added MotoRequestExample and MotoResponseExample - Added MotoRequestExample and MotoResponseExample - Added FilialRequestExample and FilialResponseExample to improve Swagger UI with realistic payloads

--- VisionHive.API/SwaggerExamples/FilialResponseExample.cs
+++ VisionHive.API/SwaggerExamples/FilialResponseExample.cs
@@ -1,6 +1,16 @@
-﻿namespace VisionHive.API.SwaggerExamples;
+﻿using Swashbuckle.AspNetCore.Filters;
+using VisionHive.Application.DTO.Response;
 
-public class FilialResponseExample
+namespace VisionHive.API.SwaggerExamples;
+
+public class FilialResponseExample : IExamplesProvider<FilialResponse>
 {
-    
+    public FilialResponse GetExamples() => new FilialResponse
+    {
+        Id = Guid.NewGuid(),
+        Nome = "Filial Zona Norte",
+        Bairro = "Santana",
+        Cnpj = "12.345.678/0001-99",
+        Patios = new List<string> { "Pátio Central", "Pátio Secundário" }
+    };
 }
