fc9644c923374c7bf67449d71709c36db7abf588
larissa557197 <larissampmuniz@gmail.com>
2025-10-28 20:41:55 -0300

    feat(api): add API versioning configuration with Asp.Versioning.Http

--- VisionHive.API/Extensions/VersioningExtensions.cs
+++ VisionHive.API/Extensions/VersioningExtensions.cs
@@ -1,6 +1,24 @@
-﻿namespace VisionHive.API.Extensions;
+﻿using Asp.Versioning;
+using Asp.Versioning.Builder;
 
-public class VersioningExtensions
+namespace VisionHive.API.Extensions;
+
+public static class VersioningExtensions
 {
-    
+    public static IServiceCollection AddVersioning(this IServiceCollection services)
+    {
+        services.AddApiVersioning(options =>
+            {
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ReportApiVersions = true;
+            })
+            .AddApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV"; // v1, v1.1, v1.1.1
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+        return services;
+    }
 }
