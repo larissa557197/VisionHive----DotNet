09d62674a214e3b18d3348371f8c29008209b100
larissa557197 <larissampmuniz@gmail.com>
2025-10-28 20:27:40 -0300

    feat(api): add UseCaseExtensions for Moto, Patio and Filial use cases

--- VisionHive.API/Extensions/UseCaseExtensions.cs
+++ VisionHive.API/Extensions/UseCaseExtensions.cs
@@ -1,6 +1,16 @@
-﻿namespace VisionHive.API.Extensions;
+﻿using VisionHive.Application.UseCases;
 
-public class UseCaseExtensions
+namespace VisionHive.API.Extensions;
+
+public static class UseCaseExtensions
 {
-    
+    public static IServiceCollection AddUseCases(this IServiceCollection services)
+    {
+        // registro dos UseCases principais
+        services.AddScoped<IMotoUseCase, MotoUseCase>();
+        services.AddScoped<IPatioUseCase, PatioUseCase>();
+        services.AddScoped<IFilialUseCase, FilialUseCase>();
+        
+        return services;
+    }
 }
