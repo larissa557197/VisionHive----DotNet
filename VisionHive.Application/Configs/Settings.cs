e32030fb17a2bd399eb1d1923b9649b74780fab2
larissa557197 <larissampmuniz@gmail.com>
2025-10-28 19:12:56 -0300

    feat(config): add DataBaseSettings with DefaultConnection property for Oracle context

--- VisionHive.Application/Configs/Settings.cs
+++ VisionHive.Application/Configs/Settings.cs
@@ -4,4 +4,6 @@ public class Settings
 {
     public SwaggerSettings Swagger { get; set; } = new();
     public MongoDbSettings MongoDb { get; set; } = new();
+    public DataBaseSettings ConnectionStrings {get; set; } = new();
+    
 }
