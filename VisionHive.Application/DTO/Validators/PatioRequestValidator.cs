926ee0208c0c6454d544824deeb623f177a85741
larissa557197 <larissampmuniz@gmail.com>
2025-10-28 18:07:29 -0300

    feat(validation): add FluentValidation validators for Moto, Patio, Filial and PaginatedRequestValidator

--- VisionHive.Application/DTO/Validators/PatioRequestValidator.cs
+++ VisionHive.Application/DTO/Validators/PatioRequestValidator.cs
@@ -1,6 +1,23 @@
-﻿namespace VisionHive.Application.DTO.Validators;
+﻿using System.Data;
+using FluentValidation;
+using VisionHive.Application.DTO.Request;
 
-public class PatioRequestValidator
+namespace VisionHive.Application.DTO.Validators;
+
+public class PatioRequestValidator : AbstractValidator<PatioRequest>
 {
-    
+    public PatioRequestValidator()
+    {
+        RuleFor(p => p.Nome)
+            .NotEmpty().WithMessage("Nome do pátio é obrigatório.")
+            .MaximumLength(100).WithMessage("Nome deve ter no máximo 100 caracteres.");
+
+        RuleFor(p => p.LimiteMotos)
+            .GreaterThan(0)
+            .WithMessage("O limite de motos deve ser maior que zero.");
+
+        RuleFor(p => p.FilialId)
+            .NotEmpty()
+            .WithMessage("O Id da filial é obrigatório.");
+    }
 }
