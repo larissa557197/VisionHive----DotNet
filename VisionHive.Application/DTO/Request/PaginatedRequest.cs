d46f85d9da209c7e87d188642c74c9a138785684
larissa557197 <larissampmuniz@gmail.com>
2025-10-28 18:12:18 -0300

    feat(dto): pagination with like subject

---
+++ VisionHive.Application/DTO/Request/PaginatedRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace VisionHive.Application.DTO.Request;
+
+public class PaginatedRequest
+{
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+    public String Subject { get; set; }
+}
