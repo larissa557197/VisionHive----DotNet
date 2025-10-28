eb7357f485dd882ef5fa31f2bbbea731e2220c0d
larissa557197 <larissampmuniz@gmail.com>
2025-10-28 18:07:49 -0300

    feat(validation): add FluentValidation validators for Moto, Patio, Filial and PaginatedRequestValidator

--- VisionHive.Application/DTO/Validators/FilialRequestValidator.cs
+++ VisionHive.Application/DTO/Validators/FilialRequestValidator.cs
@@ -1,6 +1,23 @@
-﻿namespace VisionHive.Application.DTO.Validators;
+﻿using FluentValidation;
+using VisionHive.Application.DTO.Request;
 
-public class FilialRequestValidator
+namespace VisionHive.Application.DTO.Validators;
+
+public class FilialRequestValidator : AbstractValidator<FilialRequest>
 {
-    
+    public FilialRequestValidator()
+    {
+        RuleFor(f => f.Nome)
+            .NotEmpty().WithMessage("Nome da filial é obrigatório.")
+            .MaximumLength(100).WithMessage("Nome deve ter no máximo 100 caracteres.");
+
+        RuleFor(f => f.Bairro)
+            .NotEmpty().WithMessage("O bairro é obrigatório.")
+            .MaximumLength(100).WithMessage("O bairro deve ter no máximo 100 caracteres.");
+
+        RuleFor(f => f.Cnpj)
+            .NotEmpty().WithMessage("O CNPJ é obrigatório.")
+            .Matches(@"\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}")
+            .WithMessage("Formato de CNJPJ inválido. Use o formato 00.000.000/0000-00.");
+    }
 }
