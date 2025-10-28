74f80059ab65ae8f31759f12e0381e13b791cec1
larissa557197 <larissampmuniz@gmail.com>
2025-10-28 18:18:12 -0300

    feat(validation): add FluentValidation rules for PaginatedRequest (PageNumber, PageSize, Subject)

--- VisionHive.Application/DTO/Validators/PaginatedRequestValidator.cs
+++ VisionHive.Application/DTO/Validators/PaginatedRequestValidator.cs
@@ -1,6 +1,24 @@
-﻿namespace VisionHive.Application.DTO.Validators;
+﻿using FluentValidation;
+using VisionHive.Application.DTO.Request;
 
-public class PaginatedRequestValidator
+namespace VisionHive.Application.DTO.Validators;
+
+public class PaginatedRequestValidator : AbstractValidator<PaginatedRequest>
 {
-    
+    private const int MaxPageSize = 200;
+
+    public PaginatedRequestValidator()
+    {
+        RuleFor(x => x.PageNumber)
+            .GreaterThanOrEqualTo(1)
+            .WithMessage("PageNumber deve ser maior ou igual a 1.");
+
+        RuleFor(x => x.PageSize)
+            .InclusiveBetween(1, MaxPageSize)
+            .WithMessage($"PageSize deve estar entre 1 e {MaxPageSize}.");
+
+        RuleFor(x => x.Subject)
+            .NotEmpty().WithMessage("Subject deve ser informado.")
+            .When(x => !string.IsNullOrWhiteSpace(x.Subject));
+    }
 }
