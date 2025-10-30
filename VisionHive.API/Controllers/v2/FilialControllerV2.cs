333814458601ddde155dd9b715f15a5edbe94952
larissa557197 <larissampmuniz@gmail.com>
2025-10-30 18:26:27 -0300

    feat(api-v2): add FilialControllerV2 with MongoDB CRUD endpoints

--- VisionHive.API/Controllers/v2/FilialControllerV2.cs
+++ VisionHive.API/Controllers/v2/FilialControllerV2.cs
@@ -1,6 +1,122 @@
-﻿namespace VisionHive.API.Controllers.v2;
+﻿using Microsoft.AspNetCore.Mvc;
+using VisionHive.Application.DTO.Request;
+using VisionHive.Domain.Entities;
+using VisionHive.Infrastructure.Repositories.Mongo;
 
-public class FilialControllerV2
+namespace VisionHive.API.Controllers.v2
 {
-    
+    [Route("api/v{version:apiVersion}/filiais")]
+    [ApiController]
+    [Asp.Versioning.ApiVersion(2.0)]
+    public class FilialControllerV2 : ControllerBase
+    {
+        private readonly FilialMongoRepository _repository;
+
+        public FilialControllerV2(FilialMongoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        /// <summary>
+        /// POST - Cria uma nova Filial (Mongo)
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(typeof(Filial), 200)]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> Post([FromBody] FilialRequest request)
+        {
+            if (!ModelState.IsValid)
+                return ValidationProblem(ModelState);
+
+            var filial = new Filial
+            {
+                Nome = request.Nome,
+                Bairro = request.Bairro,
+                Cnpj = request.Cnpj
+            };
+
+            await _repository.CreateAsync(filial);
+
+            return Ok(new
+            {
+                Mensagem = "Filial inserida com sucesso (MongoDB).",
+                Data = filial
+            });
+        }
+
+        /// <summary>
+        /// GET - Lista todas as  Filiais (Mongo)
+        /// </summary>
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Filial>), 200)]
+        public async Task<IActionResult> GetAll()
+        {
+            var filiais = await _repository.GetAllAsync();
+            return Ok(filiais);
+        }
+        
+        /// <summary>
+        /// GET - Busca uma Filial pelo ID (Mongo)
+        /// </summary>
+        [ProducesResponseType(typeof(Filial), 200)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var filial = await _repository.GetByIdAsync(id);
+
+            if (filial == null)
+                return NotFound(new { Mensagem = $"Filial com ID {id} não encontrada." });
+
+            return Ok(filial);
+        }
+        
+        /// <summary>
+        /// PUT - Atualiza uma filial existente (MongoDB)
+        /// </summary>
+        [HttpPut("{id:guid}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> Put(Guid id, [FromBody] FilialRequest request)
+        {
+            if (!ModelState.IsValid)
+                return ValidationProblem(ModelState);
+
+            var existente = await _repository.GetByIdAsync(id);
+            if (existente == null)
+                return NotFound(new { Mensagem = "Filial não encontrada." });
+
+            existente.Nome = request.Nome;
+            existente.Bairro = request.Bairro;
+            existente.Cnpj = request.Cnpj;
+
+            var atualizado = await _repository.UpdateAsync(existente);
+            if (!atualizado)
+                return BadRequest(new { Mensagem = "Falha ao atualizar filial." });
+
+            return Ok(new
+            {
+                Mensagem = "Filial atualizada com sucesso (MongoDB).",
+                Data = existente
+            });
+        }
+        
+        /// <summary>
+        /// DELETE - Remove uma filial existente (MongoDB)
+        /// </summary>
+        [HttpDelete("{id:guid}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var deletado = await _repository.DeleteAsync(id);
+            if (!deletado)
+                return NotFound(new { Mensagem = "Filial não encontrada." });
+
+            return Ok(new { Mensagem = "Filial excluída com sucesso (MongoDB)." });
+        }
+        
+    }
 }
+
