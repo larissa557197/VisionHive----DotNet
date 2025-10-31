8847913dda384d49b22d5de0b2cd81fb3cef3526
larissa557197 <larissampmuniz@gmail.com>
2025-10-31 00:29:40 -0300

    feat(config): create configuration classes for MongoDB, Swagger and Settings

---
+++ VisionHive.API/Controllers/v2/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace VisionHive.API.Controllers
+
+{
+    [ApiController]
+    [Route("api/v1/auth")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login(string username = "admin", string password = "1234")
+        {
+            if (username != "admin" || password != "1234")
+                return Unauthorized(new { message = "Credenciais inválidas" });
+
+            var key = Encoding.UTF8.GetBytes("super-secret-key-visionhive");
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, username) }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Ok(new { token = tokenString });
+        }
+    
+    }
+}
---
+++ VisionHive.Application/DTO/Request/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace VisionHive.Application.DTO.Request;
+
+public class LogquinRequest
+{
+    /// <summary>
+    /// Modelo simples para login (username + password).
+    /// </summary>
+}
