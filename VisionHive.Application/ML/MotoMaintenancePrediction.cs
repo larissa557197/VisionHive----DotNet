2739c6891104ed05537c16f4fb894f2b86499376
larissa557197 <larissampmuniz@gmail.com>
2025-10-31 01:19:37 -0300

    feat(ml): add ML.NET prediction endpoint for maintenance risk classification

--- VisionHive.Application/ML/MotoMaintenancePrediction.cs
+++ VisionHive.Application/ML/MotoMaintenancePrediction.cs
@@ -1,6 +1,12 @@
-﻿namespace VisionHive.Application.ML;
+﻿using Microsoft.ML.Data;
+
+namespace VisionHive.Application.ML;
 
 public class MotoMaintenancePrediction
 {
+    [ColumnName("PredictedLabel")]
+    public bool Predito { get; set; }
+    public float Score { get; set; }
     
+   
 }
